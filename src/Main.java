a6fd794c2825d4118ecdde6da157292a07d28853
student <oleksandrb3136@gmail.com>
2024-09-12 15:34:40 +0300

    first com

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    Customer customerArray [] = new Customer[5];
+    public static void main(String[] args) {
+
+    }
+    public void createCustomers(){
+        customerArray[0] = new Customer(100, "Tobi", "Edin", "Marcoo", "gray str.1", 380777, 44411111);
+        customerArray[1] = new Customer(101, "Tobi", "Ore", "Gold", "gray str.2", 380788, 44411122, 2333);
+        customerArray[2] = new Customer(102, "Fanu", "Edin", "Eren", "gray str.3", 380888, 44411123);
+        customerArray[3] = new Customer(103, "Saft", "Hadr", "Marcoo", "gray str.4", 380222, 44411221, 23);
+        customerArray[4] = new Customer(104, "Abu", "Dhabi", "Skyfall", "L. Hamilton 44", 888888, 33441000, 44);
+    }
+
+    public void printCustmers(int el []){
+        for (int i = 0; i <el.length; i ++) {
+            
+        }
+    }
+}
